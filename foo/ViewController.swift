a9aba07682f9cecf71788bb13f7e682c686ac94e
Jason Lee <huacnlee@gmail.com>
2016-07-12 17:20:54 +0800

    Initial Commit

---
+++ foo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  foo
+//
+//  Created by Jason Lee on 16/7/12.
+//  Copyright © 2016年 ruby-china. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
